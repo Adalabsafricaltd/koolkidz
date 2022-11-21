0ea84cc84ec12027c289d3d16e1a73912843a267
Chipo Hameja <chipo@adanianlabs.io>
2022-11-21 01:09:21 +0000

    Added Gallery doctype and workspace

---
+++ koolkidz/koolkidz/doctype/image_gallery/image_gallery.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Adanian Labs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Image Gallery', {
+	// refresh: function(frm) {
+
+	// }
+});
